6fa7a48e611ac739a79aee3aaf5de6698c4e09b3
pvsairamsaketh-netizen <pvsairamsaketh@gmail.com>
2026-03-15 01:03:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function startSurprise(){
+
+document.getElementById("startScreen").style.display="none";
+document.getElementById("mainContent").style.display="block";
+
+}
+
+
+
+function openLetter(){
+document.getElementById("letter").style.display="flex";
+}
+
+function closeLetter(){
+document.getElementById("letter").style.display="none";
+}
+
+
+
+let slides=document.querySelectorAll(".slide");
+
+let index=0;
+
+function showSlides(){
+
+slides.forEach(slide=>{
+slide.classList.remove("active");
+});
+
+index++;
+
+if(index>=slides.length){
+index=0;
+}
+
+slides[index].classList.add("active");
+
+}
+
+setInterval(showSlides,3000);
+
+
+
+function createHeart(){
+
+let heart=document.createElement("span");
+
+heart.innerHTML="❤";
+
+heart.style.left=Math.random()*100+"%";
+
+document.querySelector(".hearts").appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},8000);
+
+}
+
+setInterval(createHeart,700);
